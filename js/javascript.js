64c389cbaaadf4768873b3bc7a3a6cc7b0aa5eed
Ariful Islam <arifulkhadija22@gmail.com>
2026-01-27 13:52:02 +0600

    update now

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,35 @@
-document.getElementById('btn_login')
-    .addEventListener('click', function (e) {
-        e.preventDefault();
-        const phoneNumber = document.getElementById('number').value;
-        const pinNumber = document.getElementById('pin').value;
-        if (phoneNumber === '01840158154' && pinNumber === '12345') {
-            window.location.href = './home page/home.html'
-        }
-        else {
-            alert('wrong phone number or pin number')
-        }
-    })
+// document.getElementById('btn_login')
+//     .addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const phoneNumber = document.getElementById('number').value;
+//         const pinNumber = document.getElementById('pin').value;
+//         if (phoneNumber === '01840158154' && pinNumber === '12345') {
+//             window.location.href = './home page/home.html'
+//         }
+//         else {
+//             alert('wrong phone number or pin number')
+//         }
+//     })
+
+
+
+const form = document.querySelector('form');
+form.addEventListener("submit", function (e) {
+    if (!form.checkValidity) {
+        return
+    }
+
+    // document.getElementById('btn_login')
+    //     .addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const phoneNumber = document.getElementById('number').value;
+    const pinNumber = document.getElementById('pin').value;
+    if (phoneNumber === '01336134504' && pinNumber === '123456') {
+        window.location.href = './home page/home.html'
+    }
+    else {
+        alert('you are wrong enter phone number or pin number');
+    }
+
+})        
