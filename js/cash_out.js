955c6cbbdd20f7369b430612046bf02c3471ee26
Ariful Islam <arifulkhadija22@gmail.com>
2026-01-29 14:54:46 +0600

    u

---
+++ js/cash_out.js
@@ -0,0 +1,56 @@
+const cashForm = document.getElementById('cash_out_form')
+cashForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!cashForm.checkValidity()) {
+        return;
+    }
+
+    const balanceTk = document.getElementById('balance').innerText;
+    const balanceNumber = parseFloat(balanceTk);
+
+    const cashOut = document.getElementById('cash_out').value;
+    const cashNumber = parseFloat(cashOut);
+
+    const pinNumber = document.getElementById('cash_out_pin').value;
+    if (isNaN(cashNumber) || cashNumber <= 0) {
+        alert('সঠিক পরিমাণ লিখুন');
+        return;
+    }
+
+    if (cashNumber > balanceNumber) {
+        alert('পর্যাপ্ত ব্যালেন্স নেই');
+        return;
+    }
+
+
+    if (pinNumber === '123456789') {
+        const newBalance = balanceNumber - cashNumber;
+        document.getElementById('balance').innerText = newBalance;
+
+        cashForm.reset();
+    }
+    else {
+        alert('Cash out failed.Please try again')
+    }
+
+})
+
+
+const pinInput = document.getElementById('cash_out_pin');
+const tPin = document.getElementById('tPin');
+
+tPin.onclick = function () {
+    if (pinInput.type === 'password') {
+        pinInput.type = 'text';
+        tPin.innerHTML = '<i class="fa-solid fa-eye"></i>';
+    } else {
+        pinInput.type = 'password';
+        tPin.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+    }
+};
+
+
+
+
+
+
